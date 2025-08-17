2091fa1a41f8c534ef2bdd30a3473e533bc4250d
Jia Yi <s709161616@gmail.com>
2025-08-17 21:49:16 +0800

    feat: 新增 Alpine.js 通知視窗動畫效果

--- notifications/views.py
+++ notifications/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from .models import UserNotification
