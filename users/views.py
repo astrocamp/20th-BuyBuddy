96b009a477abf96d4269f62683d7c3eebff94460
custarder <custarder@hotmail.com>
2025-08-19 16:22:14 +0800

    Revert "feat#1: add user form and user address form"

--- users/views.py
+++ users/views.py
@@ -1,11 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from .models import User, UserAddress
-from .forms import UserForm, UserAddressForm
+from django.shortcuts import render
 
-def profiles(request, id):
-    user = get_object_or_404(User, pk=id)
-    user_address = get_object_or_404(UserAddress, user=user)
-    user_form = UserForm(instance=user)
-    user_address_form = UserAddressForm(instance=user_address)
-
-    return render(request, "users/profiles.html", {"user_form":user_form, "user_address_form":user_address_form})
+def index(request):
+    return render(request, "users/index.html")
