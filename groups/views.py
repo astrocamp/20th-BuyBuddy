0bef3ee29e3b78c9665131b49620e601119b8749
custarder <custarder@hotmail.com>
2025-08-19 16:16:21 +0800

    Revert "feat:#4圖片上傳功能"

--- groups/views.py
+++ groups/views.py
@@ -1,38 +1,4 @@
-from django.shortcuts import render, redirect
-from django.core.files.base import ContentFile
-from PIL import Image
-import io, os
-from .models import Group
+from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
-
-def upload(request):
-    return render(request, "groups/upload.html")
-
-def create(request):
-  if request.method == 'POST' and request.FILES.get('image'):
-    img = Image.open(request.FILES['image'])
-    img_name = os.path.splitext(request.FILES['image'].name)[0]
-    
-    if img.mode in ['RGBA', 'P']:
-      img = img.convert('RGB')
-    
-    img_io = io.BytesIO()
-    img.save(img_io, format='JPEG')
-    img_io.seek(0)
-
-    img_file = ContentFile(
-      img_io.getvalue(),
-      name=f"processed_{img_name}.jpg"
-    )
-
-    Group.objects.create(banner=img_file)
-    print('儲存:', Group.objects.all())
-    
-    return redirect('groups:read')
-  return render(request, 'groups/upload.html')
-  
-def read(request):
-  photos = Group.objects.all()
-  return render(request, 'groups/upload.html', {"photos": photos})
+    return render(request, "groups/index.html")
