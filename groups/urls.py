bb8a9dd9fb4cc720bd55d7c0b4ec466e99964d83
Qiu1996 <betty52332001@gmail.com>
2025-08-19 12:31:46 +0800

    feat:#4圖片上傳功能

--- groups/urls.py
+++ groups/urls.py
@@ -6,4 +6,7 @@ app_name = "groups"
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('upload', views.upload, name="upload"),
+    path('create', views.create, name='create'),
+    path('read', views.read, name='read'),
 ]
