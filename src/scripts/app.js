6e35be4c9804bdc20af2cd8aa7352c2d4cb62359
Qiu1996 <betty52332001@gmail.com>
2025-08-20 20:39:01 +0800

    feat: 加入Alpine.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,10 @@
 import Alpine from "alpinejs";
 import { messagesControl } from "./messages.js";
+import { createGroup } from "./create_group.js";
 
 window.Alpine = Alpine;
 Alpine.data("messagesControl", messagesControl);
+Alpine.data("createGroup", createGroup);
+
 
 Alpine.start();
---
+++ src/scripts/create_group.js
@@ -0,0 +1,14 @@
+const createGroup = () => {
+  return {
+    init(){
+      console.log(123);
+    },
+    fn(){
+      console.log(456);
+    }
+  }
+
+}
+
+
+export { createGroup };
