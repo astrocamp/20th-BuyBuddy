f94c8c1a72fd8b1028a330c09a2252acacca5fbf
Vincent Chiang <tfyu5892gash1478@gmail.com>
2025-08-22 17:04:57 +0800

    feat: 新增商品資訊

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 app_name = "products"
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path("", views.index, name="index"),
+    path("<int:product_id>/", views.product_detail, name="product_detail"),
 ]
