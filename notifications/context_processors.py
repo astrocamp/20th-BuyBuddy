50da0db40aa8b37b37f9b56e512e83240b3a9cfd
Jia Yi <s709161616@gmail.com>
2025-08-23 05:14:38 +0800

    style: 美化 navbar、登入註冊頁面

--- notifications/context_processors.py
+++ notifications/context_processors.py
@@ -5,13 +5,13 @@ def navbar_notifications(request):
     if not request.user.is_authenticated:
         return {}
 
-    notifications_query = (
+    all_user_notifications = (
         UserNotification.objects.filter(user=request.user)
         .select_related("notification")
         .order_by("-notification__created_at")
     )
 
-    unread_count = notifications_query.filter(is_read=False).count()
-    all_user_notifications = notifications_query[:5]
+    unread_count = all_user_notifications.filter(is_read=False).count()
+    # all_user_notifications = notifications_query[:5]
 
     return {"notifications": all_user_notifications, "unread_count": unread_count}
