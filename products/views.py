1ac9588e1684e348b805c63d448aca93a96eb325
Vincent Chiang <tfyu5892gash1478@gmail.com>
2025-08-22 17:04:57 +0800

    feat: 新增商品資訊

--- products/views.py
+++ products/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, ProductImage
+
 
 def index(request):
-    return render(request, "products/index.html")
+    products = Product.objects.prefetch_related("images").all()
+    return render(request, "products/index.html", {"products": products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    images = ProductImage.objects.filter(product=product)
+    return render(
+        request, "products/product_detail.html", {"product": product, "images": images}
+    )
