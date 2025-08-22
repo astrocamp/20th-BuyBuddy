87b3fd90f99dc55b72a50a9e0f43cc700115e232
Jia Yi <s709161616@gmail.com>
2025-08-22 20:31:14 +0800

    fix: 修正 groups/migrations 重名問題

--- groups/migrations/0006_alter_group_created_at.py
+++ groups/migrations/0006_alter_group_created_at.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('groups', '0005_group_description_alter_group_banner_and_more'),
+        ('groups', '0005_group_description_alter_group_banner'),
     ]
 
     operations = [
