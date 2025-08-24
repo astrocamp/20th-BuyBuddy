eadc8777efe8c60c809604c30ac01e63379232c8
Qiu1996 <betty52332001@gmail.com>
2025-08-24 20:11:30 +0800

    新增團購編輯及刪除功能

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,9 +1,9 @@
 import Alpine from "alpinejs";
 import { messagesControl } from "./messages.js";
-import { createGroup } from "./create_group.js";
+import { validators } from "./validators.js";
 
 window.Alpine = Alpine;
 Alpine.data("messagesControl", messagesControl);
-Alpine.data("createGroup", createGroup);
+Alpine.data("validators", validators);
 
 Alpine.start();
