3adacb12ca205709efad234c0f89775fccdf2138
Vincent Chiang <tfyu5892gash1478@gmail.com>
2025-08-25 11:30:59 +0800

    feat: 新增團購狀態

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Sum, F
+from products.models import Product, JoinedGroupProduct
 from .models import Product, ProductImage, Group
 from .forms import ProductForm
 from django.contrib import messages
@@ -12,24 +14,7 @@ def index(request):
 
 def product_detail(request, product_id):
     product = get_object_or_404(
-        Product.objects.select_related("group").prefetch_related("images"), pk=product_id
+        Product.objects.select_related("group").prefetch_related("images"),
+        pk=product_id,
     )
     return render(request, "products/product_detail.html", {"product": product})
-
-
-def product_create(request, group_id):
-    group = get_object_or_404(Group, pk=group_id)
-    if request.method == 'POST':
-        product_form = ProductForm(request.POST)
-        if product_form.is_valid():
-            data = product_form.save(commit=False)
-            data.group = group
-            data.save()
-            messages.success(request, "產品已建立")
-            return redirect('products:index')
-        else:
-            print(product_form.errors)
-            messages.warning(request, "欄位填寫有誤，請檢查後再試")
-            return redirect('products:product_create', group_id)
-    product_form = ProductForm()
-    return render(request, "products/product_create.html", {"product_form": product_form})
--- status/views.py
+++ status/views.py
@@ -1,4 +1,54 @@
 from django.shortcuts import render
+from products.models import Product, JoinedGroupProduct
+from django.db.models import Sum, F
+
 
 def index(request):
     return render(request, "status/index.html")
+
+
+def in_progress_list(request):
+    products = (
+        Product.objects.filter(group__status="進行中")
+        .select_related("group")
+        .prefetch_related("images")
+    )
+    for product in products:
+        member_count = product.group.joinedgroup_set.count()
+
+        product.group_status = {
+            "member_count": member_count,
+            "deadline": product.group.deadline,
+        }
+    return render(request, "status/in_progress_list.html", {"products": products})
+
+
+def ended_list(request):
+    products = (
+        Product.objects.filter(group__status="已結束")
+        .select_related("group")
+        .prefetch_related("images")
+    )
+
+    for product in products:
+        jgps = JoinedGroupProduct.objects.filter(joined_group__group=product.group)
+        total_quantity = jgps.aggregate(total=Sum("quantity"))["total"] or 0
+        total_amount = (
+            jgps.aggregate(total=Sum(F("quantity") * F("product__price")))["total"] or 0
+        )
+
+        is_successful = False
+        if (
+            product.group.goal_choice == "quantity"
+            and total_quantity >= product.group.min_quantity
+        ):
+            is_successful = True
+        elif (
+            product.group.goal_choice == "amount"
+            and total_amount >= product.group.min_amount
+        ):
+            is_successful = True
+
+        product.final_status = "已達標" if is_successful else "失敗"
+
+    return render(request, "status/ended_list.html", {"products": products})
