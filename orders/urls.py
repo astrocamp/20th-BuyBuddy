08d7a1046d7dc73e73a00073741a2228cfd3e587
Jia Yi <s709161616@gmail.com>
2025-08-26 22:18:34 +0800

    feat: 初步完成 LINE pay 串接（使用假參數）

---
+++ orders/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "orders"
+
+urlpatterns = [
+    path("checkout/", views.checkout, name="checkout"),
+    path("payment/request/", views.request, name="request"),
+    path("payment/confirm/", views.confirm, name="confirm"),
+    path("payment/cancel/", views.cancel, name="cancel"),
+]
