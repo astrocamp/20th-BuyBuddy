3cd7fa25a68635f2301d5644ce949d0129a85bc8
Jia Yi <s709161616@gmail.com>
2025-08-28 17:33:49 +0800

    feat: JoinedGroup models 新增 updated_at 欄位

--- groups/models.py
+++ groups/models.py
@@ -2,42 +2,45 @@ from django.db import models
 from django.db.models import Sum, F
 from users.models import User
 
+
 class Group(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     min_goal = models.IntegerField()
     goal_choice = models.CharField(max_length=10)
     deadline = models.DateTimeField(null=True)
-    current_progress = models.IntegerField(default=0)   
-    status = models.CharField(max_length=20)   
+    current_progress = models.IntegerField(default=0)
+    status = models.CharField(max_length=20)
     banner = models.ImageField(upload_to='groups/banners/')
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True)
     description = models.TextField(null=True)
-    total = models.IntegerField(null=True, blank=True)  
+    total = models.IntegerField(null=True, blank=True)
 
     class Meta:
         indexes = [
             models.Index(fields=["owner"]),
             models.Index(fields=["status"]),
         ]
-    
+
     def __str__(self) -> str:
         return self.name
-  
+
 
 class JoinedGroup(models.Model):
     buyer = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE) 
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     deleted_at = models.DateTimeField(null=True)
-    
+
     class Meta:
         indexes = [
-        models.Index(fields=["buyer"]),
-        models.Index(fields=["group"]),
-        models.Index(fields=["-created_at"]),
+            models.Index(fields=["buyer"]),
+            models.Index(fields=["group"]),
+            models.Index(fields=["-created_at"]),
         ]
-    
+
     def __str__(self) -> str:
         return f"{self.buyer.username} joined {self.group.name}"
+
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
