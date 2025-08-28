136a2d6c4868ccf4f70606a67cd4db6431d25d51
Jia Yi <s709161616@gmail.com>
2025-08-28 20:09:40 +0800

    feat: 新增我的訂單/我的跟團頁面

--- products/models.py
+++ products/models.py
@@ -1,21 +1,34 @@
 from django.db import models
 from groups.models import Group, JoinedGroup
 
+
 class Product(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="products")
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
 
+    @property
+    def formatted_price(self):
+        return f"${self.price:,.0f}"
+
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="images"
+    )
     url = models.ImageField(upload_to='groups/products')
     order = models.IntegerField()
 
 
 class JoinedGroupProduct(models.Model):
-    joined_group = models.ForeignKey(JoinedGroup, on_delete=models.CASCADE, related_name="joined_group_products")
+    joined_group = models.ForeignKey(
+        JoinedGroup, on_delete=models.CASCADE, related_name="joined_group_products"
+    )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     deleted_at = models.DateTimeField(null=True)
+
+    @property
+    def subtotal(self):
+        return f"${self.product.price * self.quantity:,.0f}"
