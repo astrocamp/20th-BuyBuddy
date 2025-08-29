c91030ebd4c86e094864afb47bafbb8636d45fae
Jia Yi <s709161616@gmail.com>
2025-08-29 15:03:17 +0800

    feat: 實作串接 LINE pay 金流

--- orders/urls.py
+++ orders/urls.py
@@ -4,9 +4,14 @@ from . import views
 app_name = "orders"
 
 urlpatterns = [
+    # TODO 測試付款頁面
     path("checkout/", views.checkout, name="checkout"),
-    path("payment/request/", views.request, name="request"),
-    path("payment/confirm/", views.confirm, name="confirm"),
-    path("payment/cancel/", views.cancel, name="cancel"),
+    # ===== 付款相關路徑 =====
+    path("payment/request/<int:order_id>/", views.request, name="payment_request"),
+    path("payment/confirm/", views.confirm, name="payment_confirm"),
+    path("payment/cancel/", views.cancel, name="payment_cancel"),
+    path("payment/success/", views.success, name="payment_success"),
+    path("payment/fail/", views.fail, name="payment_fail"),
+    # DEVLOG 測試建立訂單路徑
     path("test/", views.test, name="test"),
 ]
