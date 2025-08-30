eda7fea62914fbea01b6e5dbbd0ad56fdbdc63fa
Qiu1996 <betty52332001@gmail.com>
2025-08-30 22:44:06 +0800

    feat: 新增與編輯支援複數產品增加

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,4 @@ app_name = "products"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:product_id>/", views.product_detail, name="product_detail"),
-    path("product_create/<int:group_id>", views.product_create, name="product_create"),
 ]
