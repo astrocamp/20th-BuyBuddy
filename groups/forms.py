ef2eac0226f5b34181b4e1110d18f4199d2a5572
Qiu1996 <betty52332001@gmail.com>
2025-08-30 22:44:06 +0800

    feat: 新增與編輯支援複數產品增加

--- groups/forms.py
+++ groups/forms.py
@@ -1,10 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, inlineformset_factory, BaseInlineFormSet
+from django import forms
 from .models import Group
-from products.models import Product, ProductImage
+from products.models import Product
 from django.forms.widgets import *
 
 
 class GroupForm(ModelForm):
+  goal_choice = forms.ChoiceField(
+    choices=[
+          ('1', '金額'),
+          ('2', '數量')
+      ],
+      label='成團方式'
+  )
   class Meta:
     model = Group
     fields = ['name', 'banner', 'description', 'deadline', 'goal_choice', 'min_goal']
@@ -16,100 +24,25 @@ class GroupForm(ModelForm):
         'goal_choice': '成團方式',
         'min_goal': '成團目標'
     }
-    widgets = {
-      'name': TextInput(attrs={
-        'id': 'group_name',
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'placeholder': '團購名稱',
-        'required': True,
-      }),
-      'banner': FileInput(attrs={
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'placeholder': '團購圖片',
-        'required': True
-      }),
-      'description': Textarea(attrs={
-        'id': 'group_description',
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2 resize-y', 
-        'placeholder': '團購描述',
-        'rows': 5,
-        'required': True
-      }),
-      'goal_choice': Select(choices=[('amount', '金額'), ('quantity', '數量')], 
-      attrs={
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'required': True,
-      }),
-      'min_goal': NumberInput(attrs={
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'placeholder': '成團數量',
-        'min': 1,
-        'required': True,
-        'x-model': 'min_goal',
-        'x-ref': 'min_goal',
-        'x-on:input': 'removeZero()'
-      }),
-      'deadline': DateInput(attrs={
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'placeholder': '團購截止日期',
-        'type': 'date',
-        'required': True
-      }),
-    }
 
-class ProductForm(ModelForm):
-  class Meta:
-    model = Product
-    fields = ['name', 'price', 'description']
+class ProductForm(BaseInlineFormSet):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    instance = kwargs.get('instance', None)
+    if instance and instance.pk:
+      self.extra = 0
+
+ProductFormSet = inlineformset_factory(
+    Group,
+    Product,
+    fields=['name', 'price', 'description'],
+    extra=2,
+    can_delete=False,
     labels = {
       'name': '產品名稱',
       'price': '產品價格',
       'description': '產品描述'
-    }
-    widgets = {
-      'name': TextInput(attrs={
-        'id': 'product_name',
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'placeholder': '產品名稱',
-        'required': True,
-      }),
-      'price': NumberInput(attrs={
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'placeholder': '產品價格',
-        'required': True,
-        'min': 1,
-      }),
-      'description': Textarea(attrs={
-        'id': 'product_description',
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2 resize-y', 
-        'placeholder': '產品描述',
-        'rows': 5,
-        'required': True,
-      }),
-    }
-  def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # 只有在編輯現有物件時才鎖定欄位
-        if self.instance and self.instance.pk:
-            exclude_fields = ['deadline', 'min_goal']
-            for field_name, field in self.fields.items():
-                if field_name not in exclude_fields:
-                    field.widget.attrs['readonly'] = True
-                    field.widget.attrs['class'] += ' cursor-not-allowed'  
-
-
-class ProductImageForm(ModelForm):
-  class Meta:
-    model = ProductImage
-    fields = ['url']
-    labels = {
-      'url': '圖片'
-    }
-    widgets = {
-      'url': FileInput(attrs={
-        'class': 'w-full border-2 border-gray-300 rounded-md p-2', 
-        'placeholder': '圖片',
-        'required': True,
-      }),
-    }
-  
+    },
+    formset = ProductForm
+)
