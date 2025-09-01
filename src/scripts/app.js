d7ef4dbeda4798f5242286ef779254242943543f
Sheng <custarder@hotmail.com>
2025-09-01 15:18:24 +0800

    feat:上傳大頭照預覽顯示功能

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,9 +2,10 @@ import "htmx.org";
 import Alpine from "alpinejs";
 import { messagesControl } from "./messages.js";
 import { validators } from "./validators.js";
+import { avatarPreview } from "./avatarPreview.js";
 
 window.Alpine = Alpine;
 Alpine.data("messagesControl", messagesControl);
 Alpine.data("validators", validators);
-
+Alpine.data("avatarPreview", avatarPreview);
 Alpine.start();
