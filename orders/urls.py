207f18d7f2660fbbbd09c5e40b55bf62c29f7286
Jia Yi <s709161616@gmail.com>
2025-09-03 19:48:12 +0800

    feat: 付款時的選擇地址、地址新增功能

--- orders/urls.py
+++ orders/urls.py
@@ -13,11 +13,24 @@ urlpatterns = [
         name="my_orders_tab_content",
     ),
     # 確認收貨
-    path("my-orders/received/<int:order_id>/", views.received, name="received"),
+    path("my-orders/<int:order_id>/received/", views.received, name="received"),
+    # ========== 選擇收貨地址相關 ==========
+    # 選擇地址
+    path(
+        "my-orders/<int:order_id>/ship_address/",
+        views.ship_address,
+        name="ship_address",
+    ),
     # ========== 付款相關 ==========
+    # 確認訂單資訊
+    path(
+        "my-orders/<int:order_id>/check_order/",
+        views.check_order,
+        name="check_order",
+    ),
     # 請求付款
     path(
-        "my-orders/payment/request/<int:order_id>/",
+        "my-orders/<int:order_id>/payment/request/",
         views.request,
         name="payment_request",
     ),
