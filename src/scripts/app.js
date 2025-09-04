54508e446cb8438c0fbfea36012fd5c7c2e99cb9
Jia Yi <s709161616@gmail.com>
2025-09-04 14:35:35 +0800

    feat: 首頁、導覽列樣式更新

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,21 +3,19 @@ import Alpine from "alpinejs";
 import { messagesControl } from "./messages.js";
 import { validators } from "./validators.js";
 import { avatarPreview } from "./avatarPreview.js";
-import { setupTinyMCECsrf } from './tinymceCsrf.js';
+import { setupTinyMCECsrf } from "./tinymceCsrf.js";
 import { productFormset } from "./productFormset.js";
+import { navigationControl } from "./navigation.js";
 
 window.Alpine = Alpine;
 Alpine.data("messagesControl", messagesControl);
 Alpine.data("validators", validators);
 Alpine.data("avatarPreview", avatarPreview);
 Alpine.data("productFormset", productFormset);
+Alpine.data("navigationControl", navigationControl);
 
 Alpine.start();
 
-document.addEventListener('DOMContentLoaded', function() {
-    setupTinyMCECsrf();
+document.addEventListener("DOMContentLoaded", function () {
+  setupTinyMCECsrf();
 });
-
-
-
-
