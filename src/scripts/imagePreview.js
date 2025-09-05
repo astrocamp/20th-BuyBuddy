2ad797baf02641b08d083d7775fad8223f1aa22d
Jia Yi <s709161616@gmail.com>
2025-09-05 15:39:59 +0800

    style: 用戶資訊頁面 UIUX 優化

--- src/scripts/imagePreview.js
+++ src/scripts/imagePreview.js
@@ -1,18 +1,23 @@
 const imagePreview = (initialUrl) => {
-	return {
-		imageUrl: initialUrl,
-		
-		handleFile(event) {
-			const file = event.target.files[0]
-			if (file && file.type.startsWith("image/")) {
-				const reader = new FileReader()
-				reader.onload = (e) => {
-					this.imageUrl = e.target.result
-				}
-				reader.readAsDataURL(file)
-			}
-		},
-	}
-}
+  return {
+    imageUrl: initialUrl,
+    fileName: null,
 
-export { imagePreview }
+    handleFile(event) {
+      const file = event.target.files[0];
+      if (file && file.type.startsWith("image/")) {
+        // 取檔案名前十個字
+        this.fileName =
+          file.name.length > 10
+            ? file.name.substring(0, 10) + "..."
+            : file.name;
+
+        const reader = new FileReader();
+        reader.onload = (e) => (this.avatarUrl = e.target.result);
+        reader.readAsDataURL(file);
+      }
+    },
+  };
+};
+
+export { imagePreview };
