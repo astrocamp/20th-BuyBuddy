8d0abde1215b2c4d4df948946d3159cfa0bfbf3e
Qiu1996 <betty52332001@gmail.com>
2025-09-12 12:34:07 +0800

    ref: 第三方登入邏輯修正及優化

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,6 +7,7 @@ import { setupTinyMCECsrf } from "./tinymceCsrf.js";
 import { productFormset } from "./productFormset.js";
 import { backToTopBtnControl } from "./backToTopBtn.js";
 import { addressFormControl } from "./address.js";
+import { googleAuth } from "./googleAuth.js";
 
 window.Alpine = Alpine;
 Alpine.data("messagesControl", messagesControl);
@@ -19,4 +20,5 @@ Alpine.start();
 
 document.addEventListener("DOMContentLoaded", function () {
   setupTinyMCECsrf();
+  googleAuth();
 });
