6b3ab89949d2fb0b6c3817e27da62a33218f453c
Jia Yi <s709161616@gmail.com>
2025-09-19 14:39:24 +0800

    feat: 安裝 Django Debug、優化查詢

--- BuyBuddy/urls.py
+++ BuyBuddy/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from groups import views
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
-	path('tinymce/', include('tinymce.urls')),
+    path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
     path('notifications/', include("notifications.urls")),
     path('groups/', include("groups.urls")),
     path('users/', include("users.urls")),
     path('', include("pages.urls")),
     path('orders/', include("orders.urls")),
     path('accounts/', include('allauth.urls')),
-]
+] + debug_toolbar_urls()
